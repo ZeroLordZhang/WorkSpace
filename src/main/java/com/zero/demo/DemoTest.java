9933c1496aed66aa0f63a0f02f683c10b6ac0365
ZeroLordZhang <37238559+ZeroLordZhang@users.noreply.github.com>
2018-05-21 15:39:48 +0800

    Update DemoTest.java

--- src/main/java/com/zero/demo/DemoTest.java
+++ src/main/java/com/zero/demo/DemoTest.java
@@ -2,7 +2,8 @@ package com.zero.demo;
 
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("test");
+        String a = "test";
+        System.out.println("a");
 
     }
 }
